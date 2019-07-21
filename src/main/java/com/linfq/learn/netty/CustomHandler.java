b6e7cf1292652dfa26d89426b4a7b85038714f44
linfq <linfq1011@users.noreply.github.com>
2019-07-21 09:26:27 +0800

    hello netty channel生命周期。

--- src/main/java/com/linfq/learn/netty/CustomHandler.java
+++ src/main/java/com/linfq/learn/netty/CustomHandler.java
@@ -49,4 +49,63 @@ public class CustomHandler extends SimpleChannelInboundHandler<HttpObject> {
 
 	}
 
+	@Override
+	public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel。。。注册");
+		super.channelRegistered(ctx);
+	}
+
+	@Override
+	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel。。。移除");
+		super.channelUnregistered(ctx);
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel。。。活跃");
+		super.channelActive(ctx);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel。。。不活跃");
+		super.channelInactive(ctx);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel读取完毕。。。");
+		super.channelReadComplete(ctx);
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		System.out.println("用户事件触发。。。");
+		super.userEventTriggered(ctx, evt);
+	}
+
+	@Override
+	public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel可写更改");
+		super.channelWritabilityChanged(ctx);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("捕获到异常");
+		super.exceptionCaught(ctx, cause);
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("助手类添加");
+		super.handlerAdded(ctx);
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("助手类移除");
+		super.handlerRemoved(ctx);
+	}
 }
